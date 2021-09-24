88db32c16981b44a65c7cdb4b6534d373ebc1295
Павел Кулагов <kulagov@gmail.com>
2021-09-24 22:06:08 +0300

    IsAuthenticatedOrReadOnly

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -101,7 +101,7 @@ LANGUAGE_CODE = 'en-us'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
