32b8acf6f7b3b81b182c06710af3f029f6a962f7
Pavel Kulagov <kulagov@gmail.com>
2021-09-27 11:31:11 +0300

    user views permiss subsc

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -6,15 +6,16 @@ from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
-
+from rest_framework.permissions import IsAuthenticated
 from .models import Follow, User
 from .serializers import UserSerializer
 
 
 class CustomUserViewSet(UserViewSet):
     serializer_class = UserSerializer
+    permission_classes = None
 
-    @action(['get'], detail=False)
+    @action(['get'], detail=False, permission_classes=[IsAuthenticated, ])
     def subscriptions(self, request):
         follower = User.objects.filter(follow__user=request.user)
         serializer = self.get_serializer(follower, many=True)
