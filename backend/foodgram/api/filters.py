0ce4789c126c98ce76ea6d97bade117bf608cc80
Павел Кулагов <kulagov@gmail.com>
2021-09-30 22:34:27 +0300

    change filter to boolean

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -16,10 +16,10 @@ class IngredientsFilter(django_filters.FilterSet):
 class RecipesFilter(django_filters.FilterSet):
     tags = django_filters.CharFilter(
         field_name='tags__slug')
-    is_favorited = django_filters.NumberFilter(
+    is_favorited = django_filters.BooleanFilter(
         field_name='is_favorited',
         method='filter_is_favorited')
-    is_in_shopping_cart = django_filters.NumberFilter(
+    is_in_shopping_cart = django_filters.BooleanFilter(
         field_name='is_in_shopping_cart',
         method='filter_is_in_shopping_cart')
 
@@ -28,13 +28,11 @@ class RecipesFilter(django_filters.FilterSet):
         fields = ['author', 'tags', 'is_favorited', 'is_in_shopping_cart']
 
     def filter_is_favorited(self, queryset, name, value):
-        if value == 1:
+        if value:
             return queryset.filter(favlist__user=self.request.user)
         return queryset.exclude(favlist__user=self.request.user)
-        # FIXME если не 0 то выдать ошибку
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
-        if value == 1:
+        if value:
             return queryset.filter(shoppinglist__user=self.request.user)
         return queryset.exclude(shoppinglist__user=self.request.user)
-        # FIXME если не 0 то выдать ошибку
