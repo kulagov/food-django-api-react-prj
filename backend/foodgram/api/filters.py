35be13de29e62aab04d484d8189c11459b205859
Павел Кулагов <kulagov@gmail.com>
2021-09-30 20:51:10 +0300

    RecipesFilter add filters

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -16,7 +16,25 @@ class IngredientsFilter(django_filters.FilterSet):
 class RecipesFilter(django_filters.FilterSet):
     tags = django_filters.CharFilter(
         field_name='tags__slug')
+    is_favorited = django_filters.NumberFilter(
+        field_name='is_favorited',
+        method='filter_is_favorited')
+    is_in_shopping_cart = django_filters.NumberFilter(
+        field_name='is_in_shopping_cart',
+        method='filter_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
-        fields = ['author', 'tags']
+        fields = ['author', 'tags', 'is_favorited', 'is_in_shopping_cart']
+
+    def filter_is_favorited(self, queryset, name, value):
+        if value == 1:
+            return queryset.filter(favlist__user=self.request.user)
+        return queryset.exclude(favlist__user=self.request.user) 
+        # FIXME если не 0 то выдать ошибку
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if value == 1:
+            return queryset.filter(shoppinglist__user=self.request.user)
+        return queryset.exclude(shoppinglist__user=self.request.user)
+        # FIXME если не 0 то выдать ошибку
